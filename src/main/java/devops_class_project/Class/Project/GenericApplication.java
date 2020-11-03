eab8f388f46ce5610628b359b8b28d44160a1802
Prateek Rathod <emaillawrathod@gmail.com>
2020-11-03 13:30:14 +0530

    can serve pages now

--- src/main/java/devops_class_project/Class/Project/GenericApplication.java
+++ src/main/java/devops_class_project/Class/Project/GenericApplication.java
@@ -1,17 +1,13 @@
 package devops_class_project.Class.Project;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
+@SpringBootApplication
 public class GenericApplication {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
+	public static void main(String[] args) {
+		SpringApplication.run(GenericApplication.class, args);
 	}
-}
+
 }
