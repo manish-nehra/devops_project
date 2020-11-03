d1b6ba70d560ff7702d9883c6a89700e6c7be831
Prateek Rathod <emaillawrathod@gmail.com>
2020-11-03 12:40:43 +0530

    initial spring setup

---
+++ src/test/java/devops_class_project/Class/Project/GenericApplicationTests.java
@@ -0,0 +1,13 @@
+package devops_class_project.Class.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
